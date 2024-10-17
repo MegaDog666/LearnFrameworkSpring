d3cce8834a7e1214c6771f235e053156356a8035
MegaDog666 <Moonandre9999@mail.ru>
2024-10-17 23:45:27 +0300

    Lesson21
    Собственно передача данных от контроллера к представлению, дали самому решить простенькую задачу, создать калькулятор, в котором надо передать в поисковой строке 3 параметра: число А, число Б и операцию которую мы хотим сделать,
    Я сначала не понял задание и пытался сделать задание чуток по другому, почти получилось, но я там в комментарии все расписал, потом подсмотрел как он сделал в решении (В самом видео) и буду честен ну так просто я даже не думал, ну сделал настолько топорно как он сделал))
    В общем задачку я понял и лучше начал понимать GET запросы

--- src/main/java/org/example/controllers/FirstController.java
+++ src/main/java/org/example/controllers/FirstController.java
@@ -1,6 +1,7 @@
 package org.example.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,12 +13,48 @@ import jakarta.servlet.http.HttpServletRequest;
 public class FirstController {
     @GetMapping("/hello")
     public String helloPage(@RequestParam(value = "name", required = false) String name,
-                            @RequestParam(value = "surname", required = false) String surname) {
-        System.out.println("Hello " + name + " " + surname);
+                            @RequestParam(value = "surname", required = false) String surname,
+                            Model model) {
+
+//        System.out.println("Hello " + name + " " + surname);
+        model.addAttribute("message", "Hello " + name + " " + surname);
         return "/first/hello";
     }
     @GetMapping("/goodbye")
     public String goodByePage() {
         return "/first/goodbye";
+    }
+
+    @GetMapping("/calculator")
+    public String calculatorPage(@RequestParam(value = "a", required = false) int firstValue,
+                                 @RequestParam(value = "b", required = false) int secondValue,
+                                 @RequestParam(value = "operation", required = false) String operation,
+                                 Model model) {
+        // Хотел вот таким хитрым и интересным способом сделать, причем почти получилось кроме одного но, я не могу сделать этап сложения
+        // Просто потому что получая гет запрос в командной строке в виде =+ почему он не видит этот + и игнорирует его, делая строку пустой
+        // Ну и в итоге думаю как по видео стоит делать, не столь замудренно как я
+//        try {
+//            switch (operation) {
+//                case "+" -> model.addAttribute("result", Integer.parseInt(firstValue) + Integer.parseInt(secondValue));
+//                case "-" -> model.addAttribute("result", Integer.parseInt(firstValue) - Integer.parseInt(secondValue));
+//                case "*" -> model.addAttribute("result", Integer.parseInt(firstValue) * Integer.parseInt(secondValue));
+//                case "/" -> model.addAttribute("result", Integer.parseInt(firstValue) / Integer.parseInt(secondValue));
+//                default -> model.addAttribute("result", 0);
+//            }
+//        } catch (NullPointerException e) {
+//            model.addAttribute("result", 0);
+//        }
+
+        double resoult = 0;
+        switch (operation) {
+            case "addition" -> resoult = firstValue + secondValue;
+            case "subtraction" -> resoult = firstValue - secondValue;
+            case "multiplication" -> resoult = firstValue * secondValue;
+            case "division" -> resoult = (double) firstValue / secondValue;
+            default -> resoult = 0;
+        }
+
+        model.addAttribute("result", resoult);
+        return "/first/calculator";
     }
 }
