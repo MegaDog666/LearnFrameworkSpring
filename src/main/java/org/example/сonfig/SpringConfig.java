2c7b089eade16905f3ed16a9bf161b4ad0624a62
MegaDog666 <Moonandre9999@mail.ru>
2024-10-22 14:50:12 +0300

    Lesson 28
    Перешел с JDBC API на JDBC API Template, впринципе понял что к чему, удобно, но немного не понятно, ну так думаю всегда будет
    В общем все работает, логика плюс минус ясна

--- src/main/java/org/example/сonfig/SpringConfig.java
+++ src/main/java/org/example/сonfig/SpringConfig.java
@@ -5,12 +5,15 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.thymeleaf.spring6.SpringTemplateEngine;
 import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring6.view.ThymeleafViewResolver;
+import javax.sql.DataSource;
 
 @Configuration
 @ComponentScan("org.example")
@@ -38,6 +41,21 @@ public class SpringConfig implements WebMvcConfigurer {
         templateEngine.setTemplateResolver(templateResolver());
         templateEngine.setEnableSpringELCompiler(true);
         return templateEngine;
+    }
+    @Bean
+    public DataSource dateSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl("jdbc:postgresql://localhost:5433/first_db");
+        dataSource.setUsername("postgres");
+        dataSource.setPassword("1234");
+
+        return dataSource;
+    }
+    @Bean
+    public JdbcTemplate jdbcTemplate() {
+        return new JdbcTemplate(dateSource());
     }
 
     @Override
